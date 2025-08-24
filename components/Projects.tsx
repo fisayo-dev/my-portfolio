7307c69a5d07a688ef04861dd574edf5d1464651
fisayo-dev <olufisayobadina@gmail.com>
2025-08-24 22:16:01 +0100

    style: properly display content

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -58,13 +58,12 @@ const Projects = () => {
             <div className="w-full md:max-w-5xl mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {projects.reverse().map((one,index) => (
-                        <div  key={index} className="shadow-md cursor-pointer border-[0.1rem] hover:border-black/40 border-black/20 dark:hover:border-white/40 dark:border-white/20 rounded-2xl overflow-hidden">
-                            <div className="grid p-6 items-center gap-6">
+                        <div key={index} className="shadow-md cursor-pointer border-[0.1rem] hover:border-black/40 border-black/20 dark:hover:border-white/40 dark:border-white/20 rounded-2xl overflow-hidden">
+                            <div className="p-6 items-center gap-2 space-y-3">
                                 <Image src={one.image || "/next.svg"} alt={one.title} height={200} width={200} className='dark:invert'/>
-                                <div className="grid gap-1 w-full">
-                                    <h2 className="md:text-clip text-xl font-bold">{one.title}</h2>
-                                    <p className='text-sm'>{one.description}</p>
-                                </div>
+                                <h2 className="md:text-clip text-xl font-bold">{one.title}</h2>
+                                <p className='text-sm'>{one.description.length > 50 ? `${one.description.substring(0,50)}...` : one.description}</p>
+                                <span className='text-sm p-2 bg-black/20 dark:bg-white/20 rounded-full '>{one.category}</span>
                             </div>
                         </div>
                     ))}
