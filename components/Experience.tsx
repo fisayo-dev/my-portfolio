a47487fcea8a2d907b79a525496f83c6735bc3e0
fisayo-dev <olufisayobadina@gmail.com>
2025-08-24 20:59:14 +0100

    style: increase padding

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -37,7 +37,7 @@ const Experience = () => {
                 <p>For the past 5 years I have displayed excellence at these companies.</p>
             </div>
             <div className="w-full md:max-w-2xl mx-auto">
-                <div className="grid gap-4">
+                <div className="grid gap-6">
                     {experience.reverse().map((one,index) => (
                         <Link href={one.website}  key={index} className="shadow-md cursor-pointer border-[0.1rem] hover:border-black/40 border-black/20 dark:hover:border-white/40 dark:border-white/20 rounded-2xl overflow-hidden">
                             <div className="flex p-4 items-center gap-6">
