dc8e2ca198111664d867e462690ed9b221f1f24d
fisayo-dev <olufisayobadina@gmail.com>
2025-08-24 20:17:24 +0100

    style: reduce blur

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import React from 'react'
 
 const Landing = () => {
@@ -8,7 +7,8 @@ const Landing = () => {
             <div className='grid gap-8'>
                 <span className='text-sm lg:text-xl'>Welcome to my website</span>
                 <h2 className="text-4xl lg:text-6xl font-bold">I'm a Fullstack Developer</h2>
-                <div className='mx-auto h-[260px] w-[260px] md:h-[340px] md:w-[340px] rounded-full' style={{backgroundImage:  `url("pictures/Fisayo.jpg")`, backgroundPosition: `center`, backgroundSize:`cover`}}/>
+                {/* Profile Image down here */}
+                <div className='cursor-pointer mt-8 mx-auto h-[260px] w-[260px] md:h-[340px] md:w-[340px] rounded-full' style={{backgroundImage:  `url("pictures/Fisayo.jpg")`, backgroundPosition: `center`, backgroundSize:`cover`}}/>
             </div>
         </div>
     </div>
