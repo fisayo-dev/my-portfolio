392409fa87ca79dce91282b2709d2d096734bb19
fisayo-dev <olufisayobadina@gmail.com>
2025-08-24 20:22:18 +0100

    style: add header responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,15 +15,16 @@ const Header = () => {
             <div className="flex items-center justify-between py-6">
                 <div className="flex items-center space-x-8">
                     <h2 className='text-2xl font-bold'>Fisayo Obadina</h2>
-                    <div className="flex space-x-4 items-end">
+                    <div className="hidden md:flex space-x-4 items-end">
                         {headerLinks.map((link,index) => (
                             <span key={index} className='text-sm capitalize cursor-pointer dark:hover:text-white dark:text-white/70 text-black/70 hover:text-black'>{link}</span>   
                         ))}
                         </div>
                 </div>
-                <button className='flex space-x-2'>
+                <button className='hidden md:flex space-x-2'>
                     <span>Download Resume</span>
                 </button>
+                <span className='block md:hidden cursor-pointer dark:hover:text-white'>Menu</span>
             </div>
         </div>
     </div>
