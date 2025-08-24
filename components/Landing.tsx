a9bf48a205ab13bb93ecdbd9174657561745ab36
fisayo-dev <olufisayobadina@gmail.com>
2025-08-24 20:18:31 +0100

    style: make landing page more repsonsive

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -4,11 +4,11 @@ const Landing = () => {
   return (
     <div className='app-container'>
         <div className="grid place-content-center text-center h-[80vh]">
-            <div className='grid gap-8'>
-                <span className='text-sm lg:text-xl'>Welcome to my website</span>
+            <div className='grid gap-4 md:gap-8'>
+                <span className='lg:text-xl'>Welcome to my website</span>
                 <h2 className="text-4xl lg:text-6xl font-bold">I'm a Fullstack Developer</h2>
                 {/* Profile Image down here */}
-                <div className='cursor-pointer mt-8 mx-auto h-[260px] w-[260px] md:h-[340px] md:w-[340px] rounded-full' style={{backgroundImage:  `url("pictures/Fisayo.jpg")`, backgroundPosition: `center`, backgroundSize:`cover`}}/>
+                <div className='cursor-pointer mt-5 md:mt-8 mx-auto h-[260px] w-[260px] md:h-[340px] md:w-[340px] rounded-full' style={{backgroundImage:  `url("pictures/Fisayo.jpg")`, backgroundPosition: `center`, backgroundSize:`cover`}}/>
             </div>
         </div>
     </div>
