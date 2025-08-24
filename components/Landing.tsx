8c2fbfda7d0f19b17f94f32bd51fcf6efec28afd
fisayo-dev <olufisayobadina@gmail.com>
2025-08-24 20:15:20 +0100

    style: add repsonsiveness to component

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -6,9 +6,9 @@ const Landing = () => {
     <div className='app-container'>
         <div className="grid place-content-center text-center h-[80vh]">
             <div className='grid gap-8'>
-                <span className='text-xl'>Welcome to my website</span>
-                <h2 className="text-6xl font-bold">I'm a Fullstack Developer</h2>
-                <div className='mx-auto h-[400px] w-[400px] rounded-full' style={{backgroundImage:  `url("pictures/Fisayo.jpg")`, backgroundPosition: `center`, backgroundSize:`cover`}}/>
+                <span className='text-sm lg:text-xl'>Welcome to my website</span>
+                <h2 className="text-4xl lg:text-6xl font-bold">I'm a Fullstack Developer</h2>
+                <div className='mx-auto h-[260px] w-[260px] md:h-[340px] md:w-[340px] rounded-full' style={{backgroundImage:  `url("pictures/Fisayo.jpg")`, backgroundPosition: `center`, backgroundSize:`cover`}}/>
             </div>
         </div>
     </div>
